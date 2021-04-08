b82b2cab03b961eb3d6303c49b0e0b3322307a57
joeldevz <cjzm89@gmail.com>
2021-04-08 02:09:39 +0200

    test

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Head from "next/head"
 import { useState } from "react"
 import { InputEmail, InputPassword, BtnSubmit } from "../../components/app"
 import { checkPassword } from "../../functions/verification"
@@ -27,32 +28,38 @@ export default function Register() {
         location.href = "/dashboard/employeer"
     }
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="md:w-6/12 w-full px-2 py-12 m-auto bg-white rounded-md shadow-md ">
-                <div className="max-w-md w-full space-y-8 m-auto">
-                    <div>
-                        <img className="mx-auto h-20 w-auto" src="/images/icon.png" alt="Workflow" />
-                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                            Inicio Sesion</h2>
-                        <p className="mt-2 text-center text-sm text-gray-600">
-                            <Link href="auth/register">
-                                <a className="font-medium text-blue-600 hover:text-blue-500"> o Registrate y obten 1 mes gratis</a></Link>
-                        </p>
-                    </div>
-                    <form className="mt-8 space-y-6" onSubmit={loggin} action="#" method="POST">
-                        <div className="rounded-md shadow-sm -space-y-px">
-                            <InputEmail Change={saveCredentials} name="email" value={Credentials.email} placeholder="Correo Electrónico" rounded="t-md" />
-                            <InputPassword Change={saveCredentials} name="password" value={Credentials.password} placeholder="Contraseña" rounded="b-md" />
-                        </div>
-                        <p className=" text-center text-sm text-gray-600">
-                            <p className="font-medium text-red-600 hover:text-red-500">{error}</p>
-                        </p>
+        <>
+            <Head>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+            </Head>
+            <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+                <div className="md:w-6/12 w-full px-2 py-12 m-auto bg-white rounded-md shadow-md ">
+                    <div className="max-w-md w-full space-y-8 m-auto">
                         <div>
-                            <BtnSubmit icon="lock" bg="blue-600" hoverBg="blue-800" value="Iniciar Sesión" />
+                            <img className="mx-auto h-20 w-auto" src="/images/icon.png" alt="Workflow" />
+                            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                                Inicio Sesion</h2>
+                            <p className="mt-2 text-center text-sm text-gray-600">
+                                <Link href="auth/register">
+                                    <a className="font-medium text-blue-600 hover:text-blue-500"> o Registrate y obten 1 mes gratis</a></Link>
+                            </p>
                         </div>
-                    </form>
+                        <form className="mt-8 space-y-6" onSubmit={loggin} action="#" method="POST">
+                            <div className="rounded-md shadow-sm -space-y-px">
+                                <InputEmail Change={saveCredentials} name="email" value={Credentials.email} placeholder="Correo Electrónico" rounded="t-md" />
+                                <InputPassword Change={saveCredentials} name="password" value={Credentials.password} placeholder="Contraseña" rounded="b-md" />
+                            </div>
+                            <p className=" text-center text-sm text-gray-600">
+                                <p className="font-medium text-red-600 hover:text-red-500">{error}</p>
+                            </p>
+                            <div>
+                                <BtnSubmit icon="lock" bg="blue-600" hoverBg="blue-800" value="Iniciar Sesión" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
